adb3c71ca5985930505fd04127fe3e29e88adcd9
Iseul Park <shannon.iseul.p@gmail.com>
2024-06-08 12:44:38 -0400

    install cors and bodyparser to interact client side from backend

--- backend/app.js
+++ backend/app.js
@@ -1,12 +1,16 @@
 //IMPORT PACKAGE
 const express = require("express");
 const userRouter = require("./routes/user");
+const cors = require("cors");
 
 let app = express();
 
 app.use(express.json());
 
 app.use(express.static("./public"));
+
+//CORS middleware
+app.use(cors());
 
 //USING ROUTES
 app.use("/api/user", userRouter);
