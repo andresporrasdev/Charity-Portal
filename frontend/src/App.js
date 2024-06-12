017430fe985de208b75a9247877e3b77916bc2be
Iseul Park <shannon.iseul.p@gmail.com>
2024-06-11 23:39:46 -0400

    add reset password page to router

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,35 +1,37 @@
 // src/App.js
-import React from 'react';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import './App.css';
-import Nav from './components/Nav';
-import Home from './components/Home';
-import Event from './components/Event';
-import Membership from './components/Membership';
-import Volunteer from './components/Volunteer';
-import News from './components/News';
-import ContactUs from './components/ContactUs';
-import LoginForm from './components/LoginForm';
-import Register from './components/Register';
+import React from "react";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import "./App.css";
+import Nav from "./components/Nav";
+import Home from "./components/Home";
+import Event from "./components/Event";
+import Membership from "./components/Membership";
+import Volunteer from "./components/Volunteer";
+import News from "./components/News";
+import ContactUs from "./components/ContactUs";
+import LoginForm from "./components/LoginForm";
+import Register from "./components/Register";
+import ResetPassword from "./components/ResetPassword";
 
 function App() {
-    return (
-        <Router>
-            <div className="App">
-                <Nav />
-                <Routes>
-                    <Route path="/" element={<Home />} />
-                    <Route path="/event" element={<Event />} />
-                    <Route path="/membership" element={<Membership />} />
-                    <Route path="/volunteer" element={<Volunteer />} />
-                    <Route path="/news" element={<News />} />
-                    <Route path="/contact-us" element={<ContactUs />} />
-                    <Route path="/login" element={<LoginForm />} />
-                    <Route path="/register" element={<Register />} />
-                </Routes>
-            </div>
-        </Router>
-    );
+  return (
+    <Router>
+      <div className="App">
+        <Nav />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/event" element={<Event />} />
+          <Route path="/membership" element={<Membership />} />
+          <Route path="/volunteer" element={<Volunteer />} />
+          <Route path="/news" element={<News />} />
+          <Route path="/contact-us" element={<ContactUs />} />
+          <Route path="/login" element={<LoginForm />} />
+          <Route path="/register" element={<Register />} />
+          <Route path="/reset-password" element={<ResetPassword />} />
+        </Routes>
+      </div>
+    </Router>
+  );
 }
 
 export default App;
