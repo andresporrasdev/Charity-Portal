837b9873dc2f23b71e9a6596140494385c6cea9f
Iseul Park <shannon.iseul.p@gmail.com>
2024-06-11 23:39:10 -0400

    Add API calls for OTP and user registration

--- frontend/src/components/Register.js
+++ frontend/src/components/Register.js
@@ -6,6 +6,8 @@ import axios from "axios";
 const Register = () => {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
+  const [firstName, setFirstName] = useState("");
+  const [lastName, setLastName] = useState("");
   const [confirmPassword, setConfirmPassword] = useState("");
   const [emailError, setEmailError] = useState(false);
   const [passwordError, setPasswordError] = useState(false);
@@ -13,8 +15,11 @@ const Register = () => {
   const [isFocused, setIsFocused] = useState(false);
   const [showOtpModal, setShowOtpModal] = useState(false);
   const [otp, setOtp] = useState("");
+  const [otpError, setOtpError] = useState("");
   const [additionalFieldsVisible, setAdditionalFieldsVisible] = useState(false);
+  const [showNameField, setShowNameField] = useState(false);
   const [redirectUrl, setRedirectUrl] = useState("");
+  const [userData, setUserData] = useState("");
 
   const isValidEmail = (email) => {
     const emailRegex = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
@@ -51,19 +56,19 @@ const Register = () => {
       return;
     }
     console.log("email:", email);
-    // Mocking the API response
 
     try {
-      const response = await axios.post("http://localhost:3000/api/user/check", { email });
+      const response = await axios.post("http://localhost:3000/api/otp/send-otp", { email });
       console.log("response from frontend:", response);
       console.log("user data saved!");
-      //setRedirectUrl(response.data.redirectUrl);
-      //console.log("redirect url:", response.data.redirectUrl);
+      if (response.data.status === "success") {
+        setShowOtpModal(true);
+      } else if (response.data.status === "fail") {
+        setRedirectUrl(response.data.redirectUrl); // redirect to login page
+      }
     } catch (error) {
       console.error(error);
     }
-    console.log("Mock API call: User data saved!");
-    setShowOtpModal(true);
   };
 
   const handleFocus = () => {
@@ -77,41 +82,83 @@ const Register = () => {
   const handleOtpSubmit = async (e) => {
     e.preventDefault();
     console.log("OTP submitted:", otp);
-    // Mocking the API response
-    // try {
-    //   const response = await axios.post("http://localhost:3000/api/user/verify-otp", { email, otp });
-    //   console.log(response);
-    //   if (response.data.resetPassword) {
-    //     window.location.href = "/resetPassword";
-    //   } else {
-    //     setAdditionalFieldsVisible(true);
-    //     setShowOtpModal(false);
-    //   }
-    // } catch (error) {
-    //   console.error(error);
-    // }
-    console.log("Mock API call: OTP verified!");
-    // Mock response
-    const mockResponse = { resetPassword: false };
-    if (mockResponse.resetPassword) {
-      window.location.href = "/ResetPassword.js";
-    } else {
-      setAdditionalFieldsVisible(true);
-      setShowOtpModal(false);
+
+    try {
+      const [otpResponse, userResponse] = await Promise.all([
+        axios.post("http://localhost:3000/api/otp/verify-otp", { email, otp }),
+        axios.post("http://localhost:3000/api/user/check", { email }),
+      ]);
+
+      if (otpResponse.data.status === "success" && userResponse.data.status === "success") {
+        console.log("OTP verified! and you have a membership");
+        console.log("userResponse.data:", userResponse.data);
+        setUserData(userResponse.data);
+        setOtpError("");
+        setAdditionalFieldsVisible(true);
+        setShowNameField(false);
+        setShowOtpModal(false);
+      } else if (otpResponse.data.status === "success" && userResponse.data.status === "fail") {
+        console.log("OTP verified! and your a new user");
+        setAdditionalFieldsVisible(true);
+        setShowNameField(true);
+        setShowOtpModal(false);
+      } else {
+        setOtpError("Otp is not valid, please enter it again");
+        console.log("OTP verificatin is failed");
+        setOtp("");
+      }
+    } catch (error) {
+      console.error(error);
     }
   };
 
-  const handleCompleteRegistration = async () => {
-    console.log("Complete Registration");
+  const handleCompleteRegistration = async (e) => {
+    e.preventDefault();
     // Handle the completion of registration here
     if (!isValidPassword(password)) {
       console.log("Password does not meet complexity requirements");
       return;
     }
-
     if (!passwordsMatch(password, confirmPassword)) {
       console.log("Passwords do not match");
       return;
+    }
+
+    let dataToSend;
+
+    if (!userData || !userData.data) {
+      // if userData not exist, create a new user
+      dataToSend = {
+        email,
+        first_name: firstName,
+        last_name: lastName,
+        created: new Date().toISOString(),
+        isPaid: false,
+        password,
+      };
+    } else {
+      // if userData exists, use the user data from mockfile/api call
+      dataToSend = {
+        email: userData.data.email,
+        first_name: userData.data.first_name,
+        last_name: userData.data.last_name,
+        created: userData.data.created,
+        event_id: userData.data.event_id,
+        isPaid: true,
+        password,
+      };
+    }
+
+    try {
+      const response = await axios.post("http://localhost:3000/api/user/save", dataToSend);
+      if (response.data.status === "success") {
+        console.log("I am saved!!!!!!!");
+        setRedirectUrl(response.data.redirectUrl);
+      } else if (response.data.status === "fail") {
+        console.log("failed to save data");
+      }
+    } catch (error) {
+      console.error(error);
     }
   };
 
@@ -144,16 +191,21 @@ const Register = () => {
             Continue
           </button>
         )}
+
         {additionalFieldsVisible && (
           <>
-            <div className="input-box">
-              <FaUser className="icon" />
-              <input type="text" placeholder="Firstname" required />
-            </div>
-            <div className="input-box">
-              <FaUser className="icon" />
-              <input type="text" placeholder="Lastname" required />
-            </div>
+            {showNameField && (
+              <>
+                <div className="input-box">
+                  <FaUser className="icon" />
+                  <input type="text" placeholder="First Name" onChange={(e) => setFirstName(e.target.value)} required />
+                </div>
+                <div className="input-box">
+                  <FaUser className="icon" />
+                  <input type="text" placeholder="Last Name" onChange={(e) => setLastName(e.target.value)} required />
+                </div>
+              </>
+            )}
             <div className="input-box">
               <FaLock className="icon" />
               <input
@@ -187,28 +239,28 @@ const Register = () => {
             </button>
           </>
         )}
-      </div>
 
-      {/* OTP Verification Modal */}
-      {showOtpModal && (
-        <div className="otp-modal">
-          <div className="otp-modal-content">
-            <span className="close" onClick={() => setShowOtpModal(false)}>
-              &times;
-            </span>
-            <h2>Enter OTP</h2>
-            <form onSubmit={handleOtpSubmit}>
-              <div className="input-box">
-                <FaLock className="icon" />
-                <input type="text" value={otp} onChange={(e) => setOtp(e.target.value)} required />
-              </div>
-              <button className="register-button" type="submit">
-                Verify
-              </button>
-            </form>
+        {showOtpModal && (
+          <div className="otp-modal">
+            <div className="otp-modal-content">
+              <span className="close" onClick={() => setShowOtpModal(false)}>
+                &times;
+              </span>
+              <h2>Enter OTP</h2>
+              <form onSubmit={handleOtpSubmit}>
+                <div className="input-box">
+                  <FaLock className="icon" />
+                  <input type="text" value={otp} onChange={(e) => setOtp(e.target.value)} required />
+                </div>
+                {otpError && <p className="error-text">{otpError}</p>}
+                <button className="register-button" type="submit">
+                  Verify
+                </button>
+              </form>
+            </div>
           </div>
-        </div>
-      )}
+        )}
+      </div>
     </div>
   );
 };
