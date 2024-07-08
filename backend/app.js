904c8a0e89f1a06fd31b5e1605d2ebf575bc39b7
Andres Porras <camilop1730@gmail.com>
2024-07-08 18:13:32 -0400

    Added files and functions for handle volunteers

--- backend/app.js
+++ backend/app.js
@@ -4,6 +4,7 @@ const userRouter = require("./routes/user");
 const authRouter = require("./routes/auth");
 const otpRouter = require("./routes/otp");
 const eventRouter = require("./routes/event");
+const volunteerRouter = require("./routes/volunteer");
 const cors = require("cors");
 
 let app = express();
@@ -20,5 +21,6 @@ app.use("/api/user", userRouter);
 app.use("/api/otp", otpRouter);
 app.use("/api/auth", authRouter);
 app.use("/api/event", eventRouter);
+app.use("/api/volunteer", volunteerRouter);
 
 module.exports = app;
---
+++ backend/models/volunteerModel.js
@@ -0,0 +1,45 @@
+const mongoose = require('mongoose');
+
+const volunteerSchema = new mongoose.Schema({
+  name: {
+	type: String,
+	required: true,
+	trim: true,
+  },
+  email: {
+	type: String,
+	required: true,
+	trim: true,
+	lowercase: true,
+	unique: true,
+	match: /^[^\s@]+@[^\s@]+\.[^\s@]+$/,
+  },
+  contactNumber: {
+	type: String,
+	required: true,
+	trim: true,
+	match: /^[0-9]{10}$/,
+  },
+  preferredRoles: {
+	type: String,
+	required: true,
+	trim: true,
+  },
+  event: {
+	type: String,
+	required: true,
+	trim: true,
+  },
+  agreePolicies: {
+	type: Boolean,
+	required: true,
+  },
+  understandUnpaid: {
+	type: Boolean,
+	required: true,
+  },
+}, { timestamps: true });
+
+const Volunteer = mongoose.model('Volunteer', volunteerSchema);
+
+module.exports = Volunteer;
