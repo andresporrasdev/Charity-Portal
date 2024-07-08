3807d48786ea1fa6b2734558c1f68e52a9dc453f
Andres Porras <camilop1730@gmail.com>
2024-07-08 15:37:16 -0400

    Fixed Path in Volunteers.js

--- frontend/src/pages/Volunteer.js
+++ frontend/src/pages/Volunteer.js
@@ -2,7 +2,7 @@
 import React from 'react';
 import { useLocation } from 'react-router-dom';
 // import VolunteerSignUpForm from '../components/Volunteer/VolunteerSignUpForm';
-import VolunteerSignUpForm from './VolunteerSignUpForm';
+import VolunteerSignUpForm from '../components/VolunteerSignUpForm';
 
 const Volunteer = () => {
     const location = useLocation();
