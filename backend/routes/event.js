ac6c933508f169d3a27ae87a4c17c9301f168574
Andres Porras <camilop1730@gmail.com>
2024-07-14 13:08:19 -0400

    Updated API for handle CRUD operations

--- backend/routes/event.js
+++ backend/routes/event.js
@@ -4,8 +4,8 @@ const router = express.Router();
 
 router.get("/readEvent", eventController.getAllEvents);
 router.post("/addEvent", eventController.addEvent);
-router.post("/updateEvent", eventController.updateEvent);
-router.get("/deleteEvent/:id", eventController.deleteEvent);
+router.patch("/updateEvent/:id", eventController.updateEvent);
+router.delete("/deleteEvent/:id", eventController.deleteEvent);
 router.get("/getEventById/:id", eventController.getEventById);
 router.post(
   "/upload",
