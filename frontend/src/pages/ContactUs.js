fc9fb24fc66c83bb892fe0cf2c837816379eda72
Iseul Park <shannon.iseul.p@gmail.com>
2024-07-30 20:55:26 -0400

    Refactor ContactUs component and update styles

--- frontend/src/pages/ContactUs.js
+++ frontend/src/pages/ContactUs.js
@@ -1,46 +1,47 @@
-import React from 'react';
-import './Contact.css';
+import React from "react";
+import "./Contact.css";
+import { FaFacebook, FaInstagram } from "react-icons/fa";
 
 function ContactUs() {
-    return (
-        <div className="contact-container">
-            <div className="contact-info">
-                <h1>Contact us</h1>
-                <p>We would love to hear your thoughts and suggestions! Feel free to drop us an email at <strong>ottawatamilsangam@gmail.com</strong></p>
-                <p>-OR-</p>
-                <p>Follow and chat with us on social media!</p>
-                <div className="social-icons">
-                    <a href="https://www.facebook.com/TamilSangamofOttawa" target="_blank" rel="noopener noreferrer">
-                        <i className="fab fa-facebook-f"></i>
-                    </a>
-                    <a href="https://www.instagram.com/ottawatamilsangam/" target="_blank" rel="noopener noreferrer">
-                        <i className="fab fa-instagram"></i>
-                    </a>
-                </div>
-            </div>
-            <div className="contact-form">
-                <form>
-                    <div className="input-box">
-                        <label htmlFor="firstName">First Name (required)</label>
-                        <input type="text" id="firstName" placeholder="First Name" required />
-                    </div>
-                    <div className="input-box">
-                        <label htmlFor="lastName">Last Name (required)</label>
-                        <input type="text" id="lastName" placeholder="Last Name" required />
-                    </div>
-                    <div className="input-box">
-                        <label htmlFor="email">Email (required)</label>
-                        <input type="email" id="email" placeholder="Email" required />
-                    </div>
-                    <div className="input-box">
-                        <label htmlFor="message">Message (required)</label>
-                        <textarea id="message" placeholder="Your message" required></textarea>
-                    </div>
-                    <button type="submit">Send</button>
-                </form>
-            </div>
+  return (
+    <div className="contact-container">
+      <div className="contact-info">
+        <h1>Contact us</h1>
+        <p>
+          We would love to hear your thoughts and suggestions! Feel free to drop us an email at{" "}
+          <strong>ottawatamilsangam@gmail.com</strong>
+        </p>
+        <br />
+        <p>-OR-</p>
+        <p>Follow and chat with us on social media!</p>
+        <div className="social-icons">
+          <a href="https://www.facebook.com/TamilSangamofOttawa" target="_blank" rel="noopener noreferrer">
+            <FaFacebook />
+          </a>
+          <a href="https://www.instagram.com/ottawatamilsangam/" target="_blank" rel="noopener noreferrer">
+            <FaInstagram />
+          </a>
         </div>
-    );
+      </div>
+      <div className="contact-form">
+        <form>
+          <div className="input-box">
+            <label htmlFor="lastName"> Name (required)</label>
+            <input type="text" id="name" placeholder="name" required />
+          </div>
+          <div className="input-box">
+            <label htmlFor="email">Email (required)</label>
+            <input type="email" id="email" placeholder="Email" required />
+          </div>
+          <div className="input-box">
+            <label htmlFor="message">Message (required)</label>
+            <textarea id="message" placeholder="Enter your message" required></textarea>
+          </div>
+          <button type="submit">Send</button>
+        </form>
+      </div>
+    </div>
+  );
 }
 
 export default ContactUs;
